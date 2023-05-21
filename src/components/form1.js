b600e51f8fb2ce7ec1547840fbc19693645c2dcc
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-21 20:47:06 +0300

    form

--- src/components/form1.js
+++ src/components/form1.js
@@ -8,7 +8,15 @@ export class Form1 extends React.Component {
             email : '',
             theme : '',
             message : '',
-        }
+        };
+        this.handleInput=this.handleInput.bind(this);
+    }
+
+    handleInput (event)
+    {
+        this.setState(
+            {[event.target.name] : event.target.value}
+        );
     }
 
     render() {
@@ -21,16 +29,16 @@ export class Form1 extends React.Component {
                     Задай своє запитання, або повідом про порушення під час вступної кампанії
                 </div>
                 <div>
-                    <input type="text" name="firstName" placeholder="Ім'я" value={this.state.firstName} />
+                    <input type="text" name="firstName" placeholder="Ім'я" value={this.state.firstName} onChange={this.handleInput} />
                 </div>
                 <div>
-                    <input type="email" name="email" placeholder="E-mail*" value={this.state.email} />
+                    <input type="email" name="email" placeholder="E-mail*" value={this.state.email} onChange={this.handleInput} />
                 </div>
                 <div>
-                    <input type="text" name="theme" placeholder="Тема*" value={this.state.theme} />
+                    <input type="text" name="theme" placeholder="Тема*" value={this.state.theme} onChange={this.handleInput} />
                 </div>
                 <div>
-                    <textarea cols="40" rows="10" name="message" placeholder="Повідомлення" value={this.state.message} />
+                    <textarea cols="40" rows="10" name="message" placeholder="Повідомлення" value={this.state.message} onChange={this.handleInput} />
                 </div>
                 <div class="comment">
                     Поля відмічені * мають бути обов'язково заповненими
