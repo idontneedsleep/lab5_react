0a9124b0652224580228bc537327789cacfff51c
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-22 00:55:20 +0300

    required fields

--- src/components/form1.js
+++ src/components/form1.js
@@ -10,6 +10,7 @@ export class Form1 extends React.Component {
             message : '',
         };
         this.handleInput=this.handleInput.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
 
     handleInput (event)
@@ -18,24 +19,29 @@ export class Form1 extends React.Component {
             {[event.target.name] : event.target.value}
         );
     }
+    handleSubmit (event)
+    {
+        console.log(this.state.firstName + ' ' + this.state.email + ' ' + this.state.theme + ' ' + this.state.message);
+        event.preventDefault();
+    }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div class="heading">
                     Зворотній зв'язок
                 </div>
-                <div>
+                <div class="parag">
                     Задай своє запитання, або повідом про порушення під час вступної кампанії
                 </div>
                 <div>
                     <input type="text" name="firstName" placeholder="Ім'я" value={this.state.firstName} onChange={this.handleInput} />
                 </div>
                 <div>
-                    <input type="email" name="email" placeholder="E-mail*" value={this.state.email} onChange={this.handleInput} />
+                    <input type="email" name="email" placeholder="E-mail*" value={this.state.email} onChange={this.handleInput} required />
                 </div>
                 <div>
-                    <input type="text" name="theme" placeholder="Тема*" value={this.state.theme} onChange={this.handleInput} />
+                    <input type="text" name="theme" placeholder="Тема*" value={this.state.theme} onChange={this.handleInput} required />
                 </div>
                 <div>
                     <textarea cols="40" rows="10" name="message" placeholder="Повідомлення" value={this.state.message} onChange={this.handleInput} />
